a570d99bc3bd9e09771d37048e4b915a3ce01746
Thu Truong <truongthanhthu@minu.ai>
2024-01-26 19:50:50 +0700

    Fanhouse add image

--- tutorial/spiders/fanhouse_spider.py
+++ tutorial/spiders/fanhouse_spider.py
@@ -5,12 +5,14 @@ from urllib.request import urlopen
 
 import scrapy
 import xlsxwriter
+import io
+from PIL import Image
 
 
 class FanhouseSpider(scrapy.Spider):
     name = "fanhouse"
     start_urls = [
-        "https://fanhouse.waca.ec/en/product/all",
+        "https://fanhouse.waca.ec/en/product/all/newest",
     ]
 
     def parse(self, response):
@@ -18,7 +20,6 @@ class FanhouseSpider(scrapy.Spider):
         worksheet = workbook.add_worksheet()
 
         bold = workbook.add_format({"bold": 1})
-        worksheet.set_column(1, 1, 15)
         worksheet.write("A1", "No.", bold)
         worksheet.write("B1", "Title", bold)
         worksheet.write("C1", "Link", bold)
@@ -41,15 +42,31 @@ class FanhouseSpider(scrapy.Spider):
                 r"background-image: url\((.*?)\)"
             )[0]
             image_data = BytesIO(urlopen(image_url).read())
+            image = Image.open(image_data)
+            maxImageWidth = 0
+            imageWidth, imageHeight = image.size
             worksheet.write_number(row, col, row)
             worksheet.write_string(row, col + 1, item.css("span::attr(title)").get())
-            worksheet.write_string(row, col + 2, item.css("a::attr(href)").get())
+            worksheet.write_url(row, col + 2, item.css("a::attr(href)").get())
             worksheet.write_string(
                 row, col + 3, item.css(".js_origin_price::text").get()
             )
             worksheet.write_string(row, col + 4, image_url)
-            cell_position = "F{}".format(col + 5)
-            worksheet.insert_image(cell_position, image_url, {"image_data": image_data})
+            cell_position = "F{}".format(row + 1)
+            worksheet.insert_image(
+                cell_position,
+                image_url,
+                {
+                    "image_data": image_data,
+                    "x_scale": 0.5,
+                    "y_scale": 0.5,
+                    "object_position": 1,
+                },
+            )
+            if imageWidth > maxImageWidth:
+                maxImageWidth = imageWidth
+            worksheet.set_row(row, imageHeight / 2)
             row += 1
-
+        worksheet.set_column("F:F", maxImageWidth / 2)
+        worksheet.autofit()
         workbook.close()
